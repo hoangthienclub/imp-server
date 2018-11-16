c9ed07fa87fb415ee6798094fefcddc75d86a1c5
Thien Tran <thien301194@gmail.com>
2018-11-16 21:21:44 +0700

    fix

--- src/utils/populate.js
+++ src/utils/populate.js
@@ -38,11 +38,11 @@ let popCoupon = async function (schema, data) {
 	let result = await schema.populate(data, [
 		{
 			path: "couponRootId",
-			model: "CouponRoot",
-			populate: {
-				path: "banner",
-				model: "File"
-			}
+			model: "CouponRoot"
+			// populate: {
+			// 	path: "banner",
+			// 	model: "File"
+			// }
         }
     ])
 	return result;
