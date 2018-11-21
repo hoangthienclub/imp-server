295154fd458f827792b2eaf2419b4cac054e79f0
Thien Tran <thien301194@gmail.com>
2018-11-21 14:27:35 +0700

    user

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -8,17 +8,18 @@ module.exports = {
     listUser: async (req, res, next) => {
         try {
             const users = await req.dbUser.collection('users').find({
-                'company._id': req.query.companyId
+                'company._id': mongoose.Types.ObjectId(req.query.companyId)
             }).toArray();
             const userIds = users.map(user => user._id.toString());
             let levels = await req.dbMain.collection('level_current').find({uid: {$in: userIds}, status: 1, current: {$gte: 2}}).toArray();
             levels = levels.map(level => level.uid.toString());
-            const listUser = [];
+            let listUser = [];
             users.map(user => {
                 if (levels.indexOf(user._id.toString()) != -1) {
                     listUser.push(user);
                 }
             });
+            listUser = listUser.filter(user => user._id.toString() != req.user._id.toString());
             res.data = listUser;
             next();
         }
