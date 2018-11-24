5818d406295aac28c9025a011128b7af983e7ddf
Thien Tran <thien301194@gmail.com>
2018-11-24 12:16:26 +0700

    fix

--- src/databases/dbUser.js
+++ src/databases/dbUser.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 var connectDBUser = async () =>  {
 	try {
-		var client = await MongoClient.connect(`${process.env.MONGO_USER_URI}/${process.env.MONGO_USER_DB}`);
+		var client = await MongoClient.connect(`${process.env.MONGO_USER_URI}/${process.env.MONGO_ADMIN_DB}`);
 		const db = client.db(process.env.MONGO_USER_DB);
 		return db;
 	}
